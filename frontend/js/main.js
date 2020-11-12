da9a12d0205da5c41534366afe67b7a3f57b256c
MrMeeb <charles.macdonald@me.com>
2020-11-12 15:59:34 +0000

    added token checking and error handling when server unavailable

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -33,7 +33,7 @@ function toggleSlide(...args) {
       }
 }
 
-function formFeedback(location, message, type){
+function formFeedback(location, message, type) {
  
     const messageContainer = document.getElementById(location)
 
@@ -49,7 +49,7 @@ function formFeedback(location, message, type){
 
 }
 
-const fetchBackground = function() {
+function fetchBackground() {
     let fallbackNum = ["1","2","3"],
     fallbackNumChoice = fallbackNum[Math.floor(Math.random() * fallbackNum.length)];
 
@@ -72,11 +72,139 @@ gettoken = async (username, password, id) => {
         body: JSON.stringify(body)
     }
 
-    const request = await fetch('/api/gettoken', options)
-    const response = await request.json();
+    try {
 
-    localStorage.setItem('token', response)
+        const request = await fetch('/api/token/get', options)
+        const response = await request.json();
+        localStorage.setItem('token', response)
+        checktoken()
+
+    } catch (error) {
+        console.error('An error occured')
+        console.log('Retrying')
+
+        let grandparent = document.getElementById('watch-results-container')
+
+        const countdownno = function() {
+
+            var i = 4
+
+            function loop() {
+                setTimeout(function() {
+                    console.log(i)
+                    countdownval.textContent = ` ${i}`
+                    i--
+                    if (i > 0) {
+                        loop()
+                    }
+                }, 1000)
+            }
+
+            loop()
+
+        }
+
+        const countdownval100 = function() {
+
+            var i = 0
+
+            function loop() {
+                setTimeout(function() {
+                    console.log(i)
+                    progressbar.style.width = `${i}%`
+                    i++
+                    if (i < 101) {
+                        loop()
+                    }
+                }, 40)
+            }
+
+            loop()
+
+        }
+
+
+        grandparent.innerHTML = ''
+
+        let parent = document.createElement("div")
+        parent.classList.add('d-flex', 'flex-column', 'justify-contents-center', 'text-center')
+
+        let countdown = document.createElement("div")
+        countdown.textContent = 'An error occured. Page reloading in'
+
+        let countdownval = document.createElement("span")
+        countdownval.textContent = ' 5'
+
+        countdownno()
+
+        let progress = document.createElement("div")
+        progress.classList.add('progress', 'mt-2')
+
+        let progressbar = document.createElement("div")
+        progressbar.classList.add('progress-bar', 'progress-bar-pink', 'progress-bar-striped', 'progress-bar-animated')
+
+        let countdown100 = document.createElement("div")
+        countdownval100()
+
+        grandparent.appendChild(parent)
+        parent.appendChild(countdown)
+        countdown.appendChild(countdownval)
+        parent.appendChild(countdown100)
+        parent.appendChild(progress)
+        progress.appendChild(progressbar)
+
+        setTimeout(() => {
+           console.log('reloading')
+           location.reload()
+       }, 5000)
+    }
+
   
+}
+
+checktoken = async () => {
+
+    const token = localStorage.getItem('token')
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    }
+
+    if (token === null) {
+
+        console.log('Waiting for token')
+
+        setTimeout(() => {
+            checktoken()
+            return
+        }, 100);
+
+    }
+    else {
+        const request = await fetch('api/token/check', options)
+        const response = await request.json()
+    
+        console.log(response)
+
+        if (response.status === 'success') {
+
+            gettrending()
+            getwatchlist()
+            getwatchedlist()
+
+        }
+        if (response.status === 'error') {
+
+            gettoken()
+
+        }
+
+    }
+
 }
 
 function reload() {
